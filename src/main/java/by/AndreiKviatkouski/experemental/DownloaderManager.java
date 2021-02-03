86f5b21d7b34df18a355a434a5ce84a1fe6ccb6e
Andrei Kviatkouski <andruha-86@tut.by>
2021-02-03 23:41:28 +0300

    was modified method

--- src/main/java/by/AndreiKviatkouski/experemental/DownloaderManager.java
+++ src/main/java/by/AndreiKviatkouski/experemental/DownloaderManager.java
@@ -11,8 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class DownloaderManager {
 
     private List<Video> finishList;
